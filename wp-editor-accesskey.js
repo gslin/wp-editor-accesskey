3ab743f49aa34c4998247ffdb26ae630d2f97c9e
Gea-Suan Lin <gslin@gslin.org>
2019-07-04 07:36:33 +0800

    Import 0.20190704.0 version.

---
+++ wp-editor-accesskey.js
@@ -0,0 +1,27 @@
+(() => {
+    const mappings = {
+        'qt_content_code': 'c',
+        'qt_content_del': 'd',
+        'qt_content_em': 'i',
+        'qt_content_img': 'm',
+        'qt_content_ins': 's',
+        'qt_content_li': 'l',
+        'qt_content_link': 'a',
+        'qt_content_more': 't',
+        'qt_content_ol': 'o',
+        'qt_content_q': 'q',
+        'qt_content_strong': 'b',
+        'qt_content_ul': 'u',
+    };
+
+    document.addEventListener('DOMContentLoaded', () => {
+        for (let k of Object.keys(mappings)) {
+            let el = document.getElementById(k);
+            if (!el) {
+                continue;
+            }
+
+            el.setAttribute('accesskey', mappings[k]);
+        };
+    });
+})();
---
+++ wp-editor-accesskey.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+Plugin Name: WP Editor Accesskey
+Plugin URI: https://wiki.gslin.org/wiki/wp-editor-accesskey
+Description: Add accesskeys to editor.
+Author: Gea-Suan Lin
+Version: 0.20190704.0
+Author URI: https://blog.gslin.org/
+*/
+
+if (!function_exists('add_action')) {
+    exit;
+}
+
+function add_wp_editor_accesskey() {
+    $url = sprintf('%s/wp-editor-accesskey/wp-editor-accesskey.js', plugins_url());
+    wp_register_script('wp-editor-accesskey', $url, [], '0.20190704.0', true);
+    wp_enqueue_script('wp-editor-accesskey');
+}
+
+add_action('admin_enqueue_scripts', 'add_wp_editor_accesskey');
